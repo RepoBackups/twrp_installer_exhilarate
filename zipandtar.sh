f187e55fe703fddf26021481a2cda9a17b081d3d
Paul Keith <javelinanddart@gmail.com>
2015-05-04 08:57:49 -0500

    Send output to /dev/null
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- zipandtar.sh
+++ zipandtar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Zipping it up"
-zip -r recovery.zip META-INF/ recovery.img
+zip -r recovery.zip META-INF/ recovery.img > /dev/null
 echo "Done"
 
 echo "Tarring it up"
-tar -H ustar -c recovery.img > recovery.tar
-md5sum -t recovery.tar >> recovery.tar
-mv recovery.tar recovery.tar.md5
+tar -H ustar -c recovery.img > recovery.tar > /dev/null
+md5sum -t recovery.tar >> recovery.tar > /dev/null
+mv recovery.tar recovery.tar.md5 > /dev/null
 echo "Done"
