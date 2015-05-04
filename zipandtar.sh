651fbb82db195766bf2fa86fe656ee969fbd2fbb
Paul Keith <javelinanddart@gmail.com>
2015-05-04 16:31:12 -0500

    This is causing issues, remove it

--- zipandtar.sh
+++ zipandtar.sh
@@ -8,15 +8,13 @@ echo "************************************************"
 
 echo "************************************************"
 echo "Making a CWM/TWRP Flashable Zip"
-zip -r recovery.zip META-INF/ recovery.img > /dev/null
+zip -r recovery.zip META-INF/ recovery.img
 echo "Done"
 echo "************************************************"
 
 echo "************************************************"
 echo "Making an Odin Flashable Tar"
-tar -H ustar -c recovery.img > recovery.tar > /dev/null
-md5sum -t recovery.tar >> recovery.tar > /dev/null
-mv recovery.tar recovery.tar.md5 > /dev/null
+tar -H ustar -c recovery.img > recovery.tar
 echo "Done"
 echo "************************************************"
 echo "ENJOY and HAPPY FLASHING"
